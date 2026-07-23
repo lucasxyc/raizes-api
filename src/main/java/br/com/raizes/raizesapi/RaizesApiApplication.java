906848dce8f2a989357338b029bb8fa8e5d7f90d
lucasxyz <lucasnogueirae2@gmail.com>
2026-07-23 14:57:31 -0300

    Projeot Spring Boot Inicial

---
+++ src/main/java/br/com/raizes/raizesapi/RaizesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.raizes.raizesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raizes/raizesapi/RaizesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raizes.raizesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
